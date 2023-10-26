33e93aa46c2cd600475983caae81053b29d8367b
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-26 16:42:22 +0530

    committed 10/26/2023, 4:42:21 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -24,9 +24,12 @@ int is_composite(int n)
 }
 void output(int n, int result)
 {
-    if(result=0);
+    if(result==0);
     {
-    printf("%d is a composite number");
+    printf("%d is a composite number",n);
     }
     else if(result ==1)
+    {
+        printf("%d ")
+    }
 }
