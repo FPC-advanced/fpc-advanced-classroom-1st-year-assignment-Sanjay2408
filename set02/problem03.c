d68b0dee094422cf76a826918bc48a9313809466
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-28 22:35:58 +0530

    committed 10/28/2023, 10:35:57 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -23,14 +23,14 @@ int i;
 for (i=2;i<=n/2;i++)
 {if (n <=1)
 {
-    return 0;
+    return 1;
 }
  if (n%i == 0)
  {
-    return 1;
+    return 0;
  }
 else 
-return 0;
+return 1;
 }
 }
 void output(int n, int result)
