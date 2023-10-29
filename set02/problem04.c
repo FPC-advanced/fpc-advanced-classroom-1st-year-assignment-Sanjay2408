bb07cc2a78a348112d3b2d6b3fa1327568c22d56
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-29 10:42:55 +0530

    committed 10/29/2023, 10:42:55 AM

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,1 +1,47 @@
-l
+#include <stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+int main()
+{
+    int n,sum;
+    n=input_array_size();
+    int a[n];
+    input_array(n,a);
+    sum = sum_composite_numbers(n,a);
+    output(sum);
+    return 0;
+}
+int input_array_size()
+{
+ int n;
+ printf("Enter the size of array\n");
+ scanf("%d",&n);
+ return n;
+}
+void input_array(int n, int a[n])
+{
+ int i;
+ printf("Enter the array elements\n");
+ for(i=0;i<n;i++)
+ {
+    scanf("%d",&a[i]);
+ }
+}
+int sum_composite_numbers(int n, int a[n])
+{
+int i,sum=0;
+for (i=2;i<=n/2;i++)
+{
+ if (a[n]%i == 0)
+ {
+    sum =+ a[n];
+ }
+ return sum;
+}
+}
+void output(int sum)
+{
+    printf("sum of composite number is %d",sum);
+}
