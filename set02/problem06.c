f7cce80d71bfaa8b9b6a0603d51a5d42c5ab4d77
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-29 09:44:47 +0530

    committed 10/29/2023, 9:44:46 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -4,11 +4,12 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 int main()
 {
-
+    char a
+    input_string(&a);
     return 0;
 }
 void input_string(char *a)
 {
   printf("Enter a string:\n");
-  scanf("%s",a)
+  scanf("%s",a);
 }
