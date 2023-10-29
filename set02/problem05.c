d0860db4431314d6625edc094b125f5ceb727a81
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-29 09:29:47 +0530

    committed 10/29/2023, 9:29:46 AM

--- set02/problem05.c
+++ set02/problem05.c
@@ -18,6 +18,10 @@ int input()
     scanf("%d",&x);
     return x;
 }
+int find_gcd(int a, int b)
+{
+    
+}
 void output(int a, int b, int gcd)
 {
     printf("The HCF of %d and %d is %d",a,b,gcd);
