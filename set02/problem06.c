eb1812d4d90f3b24091069263922072bf877d229
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-29 09:59:23 +0530

    committed 10/29/2023, 9:59:22 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -19,8 +19,8 @@ void str_reverse(char *str, char *rev_str)
 {
 int len =0;
 for(;str[len]!='/0';len++);
-int i,j;
-for(i = len -1; i>= 0;i--)
+int i,j=0;
+for(i = len -1; i >= 0;i--)
 {
     rev_str[j] = str[i];
     j++;
