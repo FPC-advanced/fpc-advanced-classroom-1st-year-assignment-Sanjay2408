c926d9bd7cce7bb35943ea4bd592c30d52e2718e
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-29 09:51:53 +0530

    committed 10/29/2023, 9:51:52 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -5,9 +5,9 @@ void output(char *a, char *reverse_a);
 int main()
 {
     char str[100],rev_str[100],a[100],reverse_a[100];
-    input_string(&a);
-    str_reverse(&str, &rev_str);
-    output(&a,&reverse_a);
+    input_string(a);
+    str_reverse(str, rev_str);
+    output(a,reverse_a);
     return 0;
 }
 void input_string(char *a)
