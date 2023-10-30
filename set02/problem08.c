c7747e0230ad87aae42770973e5c05855da388ce
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-30 21:30:28 +0530

    committed 10/30/2023, 9:30:27 PM

--- set02/problem08.c
+++ set02/problem08.c
@@ -86,5 +86,5 @@ for (int i=0;i<n;i++)
         printf(",");
     }
 }
-printf(" is the triangle having base %.2f,height %.2f , and area %.2f \n",smallest.base,smallest.altitude,smallest.area);
+printf(" is the triangle having base %0.2f,height %0.2f , and area %0.2f \n",smallest.base,smallest.altitude,smallest.area);
 }
