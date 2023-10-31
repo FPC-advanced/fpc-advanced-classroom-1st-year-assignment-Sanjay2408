045f217642927141ae67d2dd07cc8777765bb320
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 11:11:45 +0530

    committed 10/31/2023, 11:11:44 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -15,3 +15,8 @@ void input(float *x1, float *y1, float *x2, float *y2)
     printf("Enter the values of x1,y1,x2,y2\n");
     scanf("%f %f %f %f",x1,y1,x2,y2);
 }
+float find_distance(float x1, float y1, float x2, float y2)
+{
+    float distance;
+    distance=
+}
