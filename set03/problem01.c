f4a21ed268395c10c821d8315f8ca2c2c912e19b
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 11:06:15 +0530

    committed 10/31/2023, 11:06:14 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -4,6 +4,9 @@ float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
 int main()
 {
-    float x1,x2,y1,y2;
+    float x1,x2,y1,y2,distance;
+
+    
+    output(x1,y1,x2,y2,distance);
     return 0;
 }
