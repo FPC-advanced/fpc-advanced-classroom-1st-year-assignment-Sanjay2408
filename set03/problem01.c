cf24221af80ef52cf3ecf3db209dd9c0c22ea4f0
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 12:21:54 +0530

    committed 10/31/2023, 12:21:53 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -20,7 +20,7 @@ float find_distance(float x1, float y1, float x2, float y2)
 {
     float distance;
     distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-    
+ 
  return distance;   
 }
 void output(float x1, float y1, float x2, float y2, float distance)
