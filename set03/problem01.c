fc731fa023ffba0149d9e4eae1db3f10aa5ddf6c
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 12:20:54 +0530

    committed 10/31/2023, 12:20:53 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -19,14 +19,10 @@ void input(float *x1, float *y1, float *x2, float *y2)
 float find_distance(float x1, float y1, float x2, float y2)
 {
     float distance;
-    float x,epsilon,n;
+   
     n=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
     epsilon = 0.000001;
     x=n/2;
-while((x*x-n) > epsilon || (n-x*x)> epsilon)
-{
-    distance=0.5*(x+n/x);
-}
  return distance;   
 }
 void output(float x1, float y1, float x2, float y2, float distance)
