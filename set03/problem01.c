281f30dee4bb1b9663b0aaa5c7c6e4a594ecbdc3
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 11:10:15 +0530

    committed 10/31/2023, 11:10:14 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -12,5 +12,6 @@ int main()
 }
 void input(float *x1, float *y1, float *x2, float *y2)
 {
-    printf("Enter the values of x1,y")
+    printf("Enter the values of x1,y1,x2,y2\n");
+    scanf(%f,%);
 }
