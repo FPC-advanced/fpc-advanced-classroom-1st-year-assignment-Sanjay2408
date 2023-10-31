fc41ae81083943b520dfb452d8efd81b08f903b2
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 12:04:44 +0530

    committed 10/31/2023, 12:04:43 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -18,7 +18,7 @@ void input(float *x1, float *y1, float *x2, float *y2)
 float find_distance(float x1, float y1, float x2, float y2)
 {
     float distance;
-    distance=(x2-x1)  ;
+    distance=(x2-x1)*(x2-x1)+(y2-y1)  ;
 }
 void output(float x1, float y1, float x2, float y2, float distance)
 {
