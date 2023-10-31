19c54b4769683e942b0cc5c944fc2c4ac5987dcc
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-10-31 12:08:22 +0530

    committed 10/31/2023, 12:08:21 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -20,7 +20,7 @@ float find_distance(float x1, float y1, float x2, float y2)
     float distance;
     float x,epsilon;
     x=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)
-epsilon = 0.000001;
+ epsilon = 0.000001;
 while((x*x-n) > epsilon || (n-x*x)> epsilon)
 {
     x=0.5*(x+n/x);
