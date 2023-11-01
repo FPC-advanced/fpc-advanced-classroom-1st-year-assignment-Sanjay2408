0e8fa5b097964169c3378bdaee8da725f8c87c1a
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-01 22:04:44 +0530

    committed 11/1/2023, 10:04:43 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -4,6 +4,8 @@ int is_prime(int n);
 void output(int n, int result);
 int main()
 {
+    int n,result;
+    n = 
     return 0;
 }
 int input_number()
@@ -11,4 +13,5 @@ int input_number()
     int n;
     printf("Enter a number: ");
     scanf("%d",&n);
+    return n;
 }
