051b69c58131907d9ad6d51c2dc94d5b4e30f7b3
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-01 21:33:39 +0530

    committed 11/1/2023, 9:33:38 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -39,7 +39,7 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
     }
     else
     {
-    printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f)  forms a triangle");
+    printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f)  forms a triangle",x1,y1,x2,y2,x3,y3);
     }
     
 }
