de2ded174a9263d0f51b6e8e42a097d9ff3e9d76
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-01 21:27:39 +0530

    committed 11/1/2023, 9:27:37 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -21,7 +21,7 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
     int result,a;
-    if(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3(y1-y2))==0)
+    if(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))==0)
     {
         return 1;
     }
