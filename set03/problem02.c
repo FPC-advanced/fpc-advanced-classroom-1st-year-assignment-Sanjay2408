ca4df90e65670acc247ab6f1bc1aabbe6a24f23d
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-01 19:07:21 +0530

    committed 11/1/2023, 7:07:19 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -4,9 +4,9 @@ int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
 int main()
 {
- float x1,y1,x2,y2,x3,y3;
+float x1,y1,x2,y2,x3,y3;
 int result;
-input_triangle(x1,y1,x2,y2,x3,y3);
+input_triangle(&x1,&y1,x2,y2,x3,y3);
 result = is_triangle(x1,y1,x2,y2,x3,y3);
 output(x1,y1,x2,y2,x3,y3,result);
 return 0;
