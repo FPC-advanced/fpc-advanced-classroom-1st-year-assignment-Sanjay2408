096c172ddb44d58cc155c3843ef34ccedcff3ee0
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-01 19:09:21 +0530

    committed 11/1/2023, 7:09:19 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -13,6 +13,7 @@ return 0;
 }
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
 {
-    Printf("Enter the points of the triangle\n");
-    
+    printf("Enter the points of the triangle\n");
+    scanf("%f %f",x1,y1);
+
 }
