afbead2de2e49ca149519cdaa5e00bc2edf69ab0
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 09:57:58 +0530

    committed 11/6/2023, 9:57:56 AM

--- set01/problem07.c
+++ set01/problem07.c
@@ -4,18 +4,18 @@ int sum_n_nos(int n);
 void output(int n, int sum);
 int main()
 {
-    int n,sum;
-    n=input_n();
-    sum=sum_n_nos(n);
-    output(n,sum);
-    return 0;
+ int n,sum;
+ n=input_n();
+ sum=sum_n_nos(n);
+ output(n,sum);
+ return 0;
 }
 int input_n()
 {
-    int n;
-    printf("Enter the value of n\n");
-    scanf("%d",&n);
-    return n;
+ int n;
+ printf("Enter the value of n\n");
+ scanf("%d",&n);
+ return n;
 }
 int sum_n_nos(int n)
 {
