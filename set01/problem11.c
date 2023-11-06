6f13387f2d038ea100732c10874469f4a50f9fb9
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 10:24:38 +0530

    committed 11/6/2023, 10:24:37 AM

--- set01/problem11.c
+++ set01/problem11.c
@@ -5,18 +5,17 @@ struct _complex
  float imaginary;
 };
 typedef struct _complex Complex;
-
 Complex input_complex();
 Complex add_complex(Complex a, Complex b);
 void output(Complex a, Complex b, Complex sum);
 int main()
 {
-    Complex a,b,sum;
-    a=input_complex();
-    b=input_complex();
-    sum=add_complex(a,b);
-    output(a,b,sum);
-    return 0;
+ Complex a,b,sum;
+ a=input_complex();
+ b=input_complex();
+ sum=add_complex(a,b);
+ output(a,b,sum);
+ return 0;
 }
 Complex input_complex()
 {
