ae44d16e2cd830a4c7dc57ff8639d2d49e9824d9
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 10:20:58 +0530

    committed 11/6/2023, 10:20:57 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -22,11 +22,11 @@ int stringcompare(char *string1, char *string2)
 {
     int i;
     for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
-    {
+
         if (string1[i]>string2[i])
-    {
-        return 1;
-    }
+       {
+           return 1;
+       }
     else 
     if(string1[i]<string2[i])
     {
