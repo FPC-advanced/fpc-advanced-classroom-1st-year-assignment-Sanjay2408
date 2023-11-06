2b341b3ff3fc1431727741d653a08880dd6565ab
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 10:09:28 +0530

    committed 11/6/2023, 10:09:27 AM

--- set01/problem08.c
+++ set01/problem08.c
@@ -5,20 +5,20 @@ int sum_n_array(int n, int a[n]);
 void output(int n, int a[n],int sum);
 int main()
 {
-int n,sum;
-n = input_array_size();
-int a[n];
-input_array(n,a);
-sum=sum_n_array(n,a);
-output(n,a,sum);
-return 0;
+ int n,sum;
+ n = input_array_size();
+ int a[n];
+ input_array(n,a);
+ sum=sum_n_array(n,a);
+ output(n,a,sum);
+ return 0;
 }
 int  input_array_size()
 {
-    int n;
-    printf("Enter array size\n");
-    scanf("%d",&n);
-    return n;
+ int n;
+ printf("Enter array size\n");
+ scanf("%d",&n);
+ return n;
 }
 void input_array(int n, int a[n])
 {
