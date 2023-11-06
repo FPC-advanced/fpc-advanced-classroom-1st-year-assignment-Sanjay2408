ea8fcf7e7898792287df069ceb1b3a3394a0e22d
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 09:16:15 +0530

    committed 11/6/2023, 9:16:14 AM

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c;
-    printf("Input two number a,b\n");
-    scanf("%d %d",&a,&b);
-    printf("The two numbers are %d and %d\n",a,b);
-    c=a+b;
-    printf("The sum of %d and %d is %d",a,b,c);
-    return 0;
+ int a,b,c;
+ printf("Input two number a,b\n");
+ scanf("%d%d",&a,&b);
+ printf("The two numbers are %d and %d\n",a,b);
+ c=a+b;
+ printf("The sum of %d and %d is %d",a,b,c);
+ return 0;
 }
