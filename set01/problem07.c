0f08f37cd8d5535d65ae3ad6768da5d41987a5cb
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 09:58:28 +0530

    committed 11/6/2023, 9:58:26 AM

--- set01/problem07.c
+++ set01/problem07.c
@@ -19,14 +19,14 @@ int input_n()
 }
 int sum_n_nos(int n)
 {
-    int i=1,sum=0;
-    for(i=1;i<=n;i++)
-    {
-        sum=sum+i;
-    }
-    return sum;
+ int i=1,sum=0;
+ for(i=1;i<=n;i++)
+ {
+     sum=sum+i;
+ }
+ return sum;
 }
 void output(int n,int sum)
 {
-    printf("the sum of %d numbers is %d",n,sum);
+ printf("the sum of %d numbers is %d",n,sum);
 }
