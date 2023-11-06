a7b596980e821efc8d8c41d091c13c9758084c39
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 09:18:15 +0530

    committed 11/6/2023, 9:18:14 AM

--- set01/problem03.c
+++ set01/problem03.c
@@ -4,24 +4,24 @@ int add(int a,int b );
 void output( int sum,int a ,int b );
 int main()
 {
-    int a,b,sum,x;
-    a=input(x);
-    b=input(x);
-    sum=add(a,b);
-    output(sum,a,b);
-    return 0;
+ int a,b,sum,x;
+ a=input(x);
+ b=input(x);
+ sum=add(a,b);
+ output(sum,a,b);
+ return 0;
 }
 int input(int x)
 {
-    printf("Input a Number\n");
-    scanf("%d",&x);
-    return x;
+ printf("Input a Number\n");
+ scanf("%d",&x);
+ return x;
 }
 int add(int a,int b)
 {
-    int sum;
-    sum=a+b;
-    return sum;
+ int sum;
+ sum=a+b;
+ return sum;
 }
 void output(int  sum,int a,int b)
 {
