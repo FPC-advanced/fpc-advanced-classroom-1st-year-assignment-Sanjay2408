c2d5f2868be1287e08544e2993fb68eb2fdfb350
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-06 10:19:58 +0530

    committed 11/6/2023, 10:19:57 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -4,19 +4,19 @@ int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 int main()
 {
-    int result;
-    char string1[100],string2[100];
-    input_two_strings(string1,string2);
-    result=stringcompare(string1,string2);
-    output(string1,string2,result);
-    return 0;
+ int result;
+ char string1[100],string2[100];
+ input_two_strings(string1,string2);
+ result=stringcompare(string1,string2);
+ output(string1,string2,result);
+ return 0;
 }
 void input_two_strings(char *string1, char *string2)
 {
-    printf("Enter the first string\n");
-    scanf("%s",string1);
-    printf("Enter the second string\n");
-    scanf("%s",string2);
+ printf("Enter the first string\n");
+ scanf("%s",string1);
+ printf("Enter the second string\n");
+ scanf("%s",string2);
 }
 int stringcompare(char *string1, char *string2)
 {
