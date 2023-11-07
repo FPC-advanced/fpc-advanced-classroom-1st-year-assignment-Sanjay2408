de25d63bd42e01cff5bc3d3355d7f76a9425f868
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 07:17:17 +0530

    committed 11/7/2023, 7:17:15 AM

--- set02/problem05.c
+++ set02/problem05.c
@@ -23,12 +23,12 @@ int find_gcd(int a, int b)
  int gcd,i;
  for(i=1;i<=a && i<=b;i++)
   {
-      if(a%i == 0 && b%i ==0)
-      gcd  = i;
+    if(a%i == 0 && b%i ==0)
+    gcd  = i;
   }
-  return gcd;
+   return gcd;
 }
 void output(int a, int b, int gcd)
 {
-    printf("The HCF of %d and %d is %d",a,b,gcd);
+ printf("The HCF of %d and %d is %d",a,b,gcd);
 }
