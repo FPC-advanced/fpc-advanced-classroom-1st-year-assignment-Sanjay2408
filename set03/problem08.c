5dd7db002a498a67b000bdd3b66c73ee2e6059d7
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 11:34:00 +0530

    committed 11/7/2023, 11:33:59 AM

--- set03/problem08.c
+++ set03/problem08.c
@@ -32,6 +32,7 @@ int input_polygon(Polygon *p)
  int i;
   for(i=0;i<p;i++)
   {
-    printf("Enter the coordinates of point %d");
+    printf("Enter the coordinates of point %d (x,y)",i);
+    scanf(%d,)
   }
 }
