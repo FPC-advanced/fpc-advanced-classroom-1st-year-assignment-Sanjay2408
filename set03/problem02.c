71b3350a9711f0629b377111ceba5d4b3f947325
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 11:00:32 +0530

    committed 11/7/2023, 11:00:31 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -13,24 +13,24 @@ return 0;
 }
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
 {
-    printf("Enter the points of the triangle\n");
-    scanf("%f %f",x1,y1);
-    scanf("%f %f",x2,y2);
-    scanf("%f %f",x3,y3);
+ printf("Enter the points of the triangle\n");
+ scanf("%f %f",x1,y1);
+ scanf("%f %f",x2,y2);
+ scanf("%f %f",x3,y3);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-    float area;
-    area=(0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2 ) ) );
-    if(area==0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-    
+ float area;
+ area=(0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2 ) ) );
+ if(area==0)
+  {
+    return 1;
+  }
+ else
+  {
+    return 0;
+  }
+ 
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
 {
