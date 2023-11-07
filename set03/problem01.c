8e7357cee948502f41e2e623906eea3ee0e94534
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 10:59:27 +0530

    committed 11/7/2023, 10:59:27 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -5,25 +5,24 @@ float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
 int main()
 {
-    float x1,x2,y1,y2,distance;
-    input(&x1,&y1,&x2,&y2);
-    distance=find_distance(x1,y1,x2,y2);
-    output(x1,y1,x2,y2,distance);
-    return 0;
+ float x1,x2,y1,y2,distance;
+ input(&x1,&y1,&x2,&y2);
+ distance=find_distance(x1,y1,x2,y2);
+ output(x1,y1,x2,y2,distance);
+ return 0;
 }
 void input(float *x1, float *y1, float *x2, float *y2)
 {
-    printf("Enter the values of x1,y1,x2,y2\n");
-    scanf("%f %f %f %f",x1,y1,x2,y2);
+ printf("Enter the values of x1,y1,x2,y2\n");
+ scanf("%f %f %f %f",x1,y1,x2,y2);
 }
 float find_distance(float x1, float y1, float x2, float y2)
 {
-    float distance;
-    distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
- 
+ float distance;
+ distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
  return distance;   
 }
 void output(float x1, float y1, float x2, float y2, float distance)
 {
-    printf("The distance between the points(%f,%f) and (%f,%f) is %f",x1,y1,x2,y2,distance);
+ printf("The distance between the points(%f,%f) and (%f,%f) is %f",x1,y1,x2,y2,distance);
 }
