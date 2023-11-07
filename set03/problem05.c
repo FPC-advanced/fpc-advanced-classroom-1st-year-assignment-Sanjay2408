9944429d4982c0184d8053bb583320924b85a274
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 19:26:54 +0530

    committed 11/7/2023, 7:26:53 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -23,13 +23,13 @@ void init_array(int n, int a[n])
     printf("Enter the array elements:");
     for(i=0;i<n;i++)
     {
-     scanf("%d",a[i]);
+     scanf("%d",&a[i]);
     }
 }
 void erotosthenes_sieve(int n, int a[n])
 {
-    int i;
- for(int i=2;i<=n;i++)
+ int i;
+ for(i=2;i*i<=n;i++)
  {
   if()
  }
