fc393724a544ee38fb612a325af392a124407bf9
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 11:02:32 +0530

    committed 11/7/2023, 11:02:31 AM

--- set03/problem03.c
+++ set03/problem03.c
@@ -26,22 +26,21 @@ int is_prime(int n)
   }
  for(i=2;i*i<=n;i++)
  {
-     if(n%i== 0)
-     {
-     return 0;
-     }
-    
+  if(n%i== 0)
+   {
+    return 0;
+   } 
  }
- return 1;
+  return 1;
 }
 void output(int n, int result)
 {
-    if (result==1)
-    {
-    printf("%d is a prime number",n);
-    }
-    else
-    {
-        printf("%d is not a prime number",n);
-    }
+ if (result==1)
+ {
+ printf("%d is a prime number",n);
+ }
+ else
+ {
+     printf("%d is not a prime number",n);
+ }
 }
