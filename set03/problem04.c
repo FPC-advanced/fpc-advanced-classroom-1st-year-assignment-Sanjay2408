fc4cdd362d2d7e2522414bb6fddd18607abd616a
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 11:19:32 +0530

    committed 11/7/2023, 11:19:31 AM

--- set03/problem04.c
+++ set03/problem04.c
@@ -4,40 +4,40 @@ int find_fibo(int n);
 void output(int n, int fibo);
 int main()
 {
-    int n,fibo;
-    n = input();
-    fibo = find_fibo(n);
-    output(n,fibo);
-    return 0;
+ int n,fibo;
+ n = input();
+ fibo = find_fibo(n);
+ output(n,fibo);
+ return 0;
 }
 int input()
 {
-    int n;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    return n;
+ int n;
+ printf("Enter a number:");
+ scanf("%d",&n);
+ return n;
 }
 int find_fibo(int n)
 {
-int fibo,i,x1=0,x2=1;
-for(i=1;i<=n;i++)
-{
-    if(i<=1)
-    {
-     fibo=0;
-    }
-else
-    if(i<=2 && i>1)
-    {
-        fibo=1;
-    }
-    else{
-        fibo = x1 + x2;
-        x1 = x2;
-        x2 = fibo;
-    }
-}
-return fibo;
+ int fibo,i,x1=0,x2=1;
+  for(i=1;i<=n;i++)
+   {
+     if(i<=1)
+     {
+      fibo=0;
+     }
+ else
+     if(i<=2 && i>1)
+     {
+         fibo=1;
+     }
+     else{
+         fibo = x1 + x2;
+         x1 = x2;
+         x2 = fibo;
+     }
+ }
+ return fibo;
 }
 void output(int n,int fibo)
 {
