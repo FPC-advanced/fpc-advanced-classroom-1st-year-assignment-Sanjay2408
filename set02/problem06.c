b147f7907037d56e26cdcce4a8157f5be52f54ab
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 07:19:17 +0530

    committed 11/7/2023, 7:19:15 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -4,22 +4,22 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 int main()
 {
-    char str[100],rev_str[100],a[100],reverse_a[100];
-    input_string(a);
-    int i;
-    for(i=0;a[i] != '\0';i++)
-    {
-        str[i]=a[i];
-    }
-    str[i]='\0';
-    str_reverse(str, rev_str);
-    output(a,rev_str);
-    return 0;
+ char str[100],rev_str[100],a[100],reverse_a[100];
+ input_string(a);
+ int i;
+ for(i=0;a[i] != '\0';i++)
+ {
+     str[i]=a[i];
+ }
+ str[i]='\0';
+ str_reverse(str, rev_str);
+ output(a,rev_str);
+ return 0;
 }
 void input_string(char *a)
 {
-  printf("Enter a string:\n");
-  scanf("%s",a);
+ printf("Enter a string:\n");
+ scanf("%s",a);
 }
 void str_reverse(char *str, char *rev_str)
 {
