5f3794a8c8d2fd1b11bb2185e3ca6d8927460479
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 11:01:02 +0530

    committed 11/7/2023, 11:01:01 AM

--- set03/problem02.c
+++ set03/problem02.c
@@ -30,17 +30,16 @@ int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
   {
     return 0;
   }
- 
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
 {
-    if (result==1)
-    {
-    printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f) do not form a triangle\n",x1,y1,x2,y2,x3,y3);
-    }
-    else
-    {
-    printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f)  forms a triangle\n",x1,y1,x2,y2,x3,y3);
-    }
+ if (result==1)
+  {
+   printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f) do not form a triangle\n",x1,y1,x2,y2,x3,y3);
+  }
+ else
+  {
+   printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f)  forms a triangle\n",x1,y1,x2,y2,x3,y3);
+  }
     
 }
