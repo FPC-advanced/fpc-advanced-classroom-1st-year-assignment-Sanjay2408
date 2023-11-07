3f0cacfe06f748776e888e84b3a27f922552b131
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 19:30:24 +0530

    committed 11/7/2023, 7:30:23 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -43,11 +43,11 @@ void erotosthenes_sieve(int n, int a[n])
 void output(int n, int a[n])
 {
  for(int i=2;i<=n;i++)
-{
-    if(a[i] != 0)
-    {
-        printf("%d",i);
-    }
-}
-
+ {
+  if(a[i] != 0)
+   {
+    printf("%d",i);
+   }
+ }
+ 
 }
