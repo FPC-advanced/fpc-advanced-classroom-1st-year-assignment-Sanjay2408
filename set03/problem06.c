98dbc45c1b68d17784bae34d7c3c2d69ca4e2a7a
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-07 19:44:21 +0530

    committed 11/7/2023, 7:44:20 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
+int main()
+{
+ 
+
+ return 0;
+}
