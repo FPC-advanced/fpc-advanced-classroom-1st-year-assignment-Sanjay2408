4cbe542653cc9b098fed55cc4c61e5e9f4d21aeb
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-10 13:43:26 +0530

    committed 11/10/2023, 1:43:24 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -4,12 +4,12 @@ int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 int main()
 {
- int result;
- char string1[100],string2[100];
- input_two_strings(string1,string2);
- result=stringcompare(string1,string2);
- output(string1,string2,result);
- return 0;
+  int result;
+  char string1[100],string2[100];
+  input_two_strings(string1,string2);
+  result=stringcompare(string1,string2);
+  output(string1,string2,result);
+  return 0;
 }
 void input_two_strings(char *string1, char *string2)
 {
