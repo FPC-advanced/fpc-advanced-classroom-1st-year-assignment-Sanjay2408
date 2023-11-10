1c49b6a877fe76ca5109cfb5d8e8620cad1ace75
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-10 13:19:37 +0530

    committed 11/10/2023, 1:19:37 PM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-int input(int *a, int *b, int *c);
+void input(int *a, int *b, int *c);
 void compare(int a, int b, int c, int *largest);
 void output(int a, int b, int c, int largest);
 int main()
 { 
  int a,b,c,x,largest;
- x=input(&a,&b,&c);
+ input(&a,&b,&c);
  compare(a,b,c,&largest);
  output(a,b,c,largest);
  return 0;
 }
-int input(int *a, int *b, int *c)
+void input(int *a, int *b, int *c)
 {
  int x;
  printf("Enter three numbers\n");
  scanf("%d%d%d",a,b,c);
- return 0;
 }
 void compare(int a, int b, int c, int *largest)
 {
