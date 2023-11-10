2a6323b1f5c2be526c2d0b1b131f1fc69cc43b32
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-10 13:15:00 +0530

    committed 11/10/2023, 1:14:59 PM

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-int input(int x );
+int input();
 int add(int a,int b );
 void output( int sum,int a ,int b );
 int main()
 {
  int a,b,sum,x;
- a=input(x);
- b=input(x);
+ a=input();
+ b=input();
  sum=add(a,b);
  output(sum,a,b);
  return 0;
 }
-int input(int x)
+int input()
 {
+ int x;   
  printf("Input a Number\n");
  scanf("%d",&x);
  return x;
@@ -25,5 +26,5 @@ int add(int a,int b)
 }
 void output(int sum,int a,int b)
 {
-    printf("The sum of %d and %d is %d",a,b,sum);
+ printf("The sum of %d and %d is %d",a,b,sum);
 }
