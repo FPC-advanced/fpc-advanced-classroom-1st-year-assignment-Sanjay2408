7aad25592d730c215a891df7356db72032e421d7
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-11 08:19:04 +0530

    committed 11/11/2023, 8:19:03 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -24,5 +24,5 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
 }
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
 {
-    printf("")
+    printf("num1/den1 + num2/den2 = res_num/ ")
 }
