f1cfb922484ae1edb29b6509e3c1a3f8df481498
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-11 08:15:34 +0530

    committed 11/11/2023, 8:15:33 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -5,7 +5,7 @@ void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
 int main()
 {
 int num1,den1,num2,den2,res_num,res_den;
-
+input(&num1,&den1,&num2,den2)
 return  0;
 }
 void input(int *num1, int *den1, int *num2, int *den2)
