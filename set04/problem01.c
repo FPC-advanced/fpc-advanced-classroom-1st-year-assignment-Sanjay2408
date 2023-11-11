a3b97e7d3832b2c3b3584d756abfead5e5afbf45
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-11 08:23:04 +0530

    committed 11/11/2023, 8:23:03 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -14,7 +14,7 @@ void input(int *num1, int *den1, int *num2, int *den2)
 {
     printf("Enter the numerator and denominator of first fraction:\n");
     scanf("%d %d",num1,den1);
-    printf("Enter the numerator and denominator of first fraction:\n");
+    printf("Enter the numerator and denominator of second fraction:\n");
     scanf("%d %d",num2,den2);
 }
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
