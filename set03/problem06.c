11ab6b032adc91cc6a8e9ecda4239f0528b2298a
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-11 07:44:46 +0530

    committed 11/11/2023, 7:44:45 AM

--- set03/problem06.c
+++ set03/problem06.c
@@ -20,12 +20,12 @@ void input_string(char* a, char* b)
 }
 int sub_str_index(char* string, char* substring)
 {
-    int i,j;
-    for(i=0;string[i] != '\0;i++')
-    {
-        for(j=0;substring[j] != '\0' && string[i+j]== substring[j];j++);
-        if(substring[j]=='\0')
-        return i;
-    }
-    return -1;
+ int i,j;
+ for(i=0;string[i] != '\0;i++')
+ {
+  for(j=0;substring[j] != '\0' && string[i+j]== substring[j];j++);
+  if(substring[j]=='\0')
+  return i;
+ }
+ return -1;
 }
