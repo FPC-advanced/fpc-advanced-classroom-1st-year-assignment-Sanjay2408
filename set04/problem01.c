e205e8d1e38e9460ea1a7969eda9a2353e6a14e4
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-11 08:15:04 +0530

    committed 11/11/2023, 8:15:03 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -5,11 +5,13 @@ void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
 int main()
 {
 int num1,den1,num2,den2,res_num,res_den;
+
+return  0;
 }
 void input(int *num1, int *den1, int *num2, int *den2)
 {
     printf("Enter the numerator and denominator of first fraction:\n");
-    scanf("%d %d",num1, den1)
+    scanf("%d %d",num1,den1);
     printf("Enter the numerator and denominator of first fraction:\n");
-
+    scanf("%d %d",num2,den2);
 }
