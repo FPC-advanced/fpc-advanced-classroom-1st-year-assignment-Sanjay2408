e336a697b18afe31b3ed8fa638ac32d5b408daf8
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-15 16:16:31 +0530

    committed 11/15/2023, 4:16:30 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -34,6 +34,8 @@ int sub_str_index(char* string, char* substring)
 void output(char *string, char *substring, int index)
 {
     if (index != -1)
+    {
+     printf("The index of %s in %s is %d\n",substring ,string,index);
+    }
     
- printf("The index of %s in %s is %d\n",substring ,string,index);
 }
