030828d6a950ee769adc3141673701337b40efa2
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-15 16:08:27 +0530

    committed 11/15/2023, 4:08:25 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -24,7 +24,7 @@ int sub_str_index(char* string, char* substring)
  for(i=0;string[i] != '\0';i++)
  {
   for(j=0;substring[j] != '\0' && string[i+j]== substring[j];j++);
-  if(substring[j]==string[i+j])
+  if(substring[j]==string[i])
   {
   return i;
   }
