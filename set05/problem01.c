ffeb5380d58d619ab3288c214967ce3dfb4e830f
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-15 21:15:24 +0530

    committed 11/15/2023, 9:15:23 PM

--- set05/problem01.c
+++ set05/problem01.c
@@ -1,1 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h><
+struct _point {
+  float x;
+  float y;
+};
+
+typedef struct _point Point;
+
+Point input();
+void dist(Point a, Point b, float *res);
+void output(Point a, Point b, float res);
+int main()
+{
+    return 0;
+}
+Point input()
+{
+    Point a,b;
+    printf("Enter two numbers:");
+}
