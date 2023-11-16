a0cc418afd2de556fa9db41a27dc43766d13518c
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-16 14:16:23 +0530

    committed 11/16/2023, 2:16:22 PM

--- set05/problem02.c
+++ set05/problem02.c
@@ -3,12 +3,13 @@
 void input_camel_details(float *radius, float *height, float *length);
 float find_weight(float radius, float height, float length);
 void output(float radius, float height, float length, float weight);
-int main
+int main()
 {
+    
     return 0;
 }
 void input_camel_details(float *radius, float *height, float *length)
 {
  printf("Enter the radius,height and length of the Camel:\n");
- scanf("%f %f %f,radius,height")
+ scanf("%f %f %f,radius,height,length");
 }
