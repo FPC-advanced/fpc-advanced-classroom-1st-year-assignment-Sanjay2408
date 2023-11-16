9043fa4d8437f7158ff3d0f2750116f59d19f08c
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-16 14:52:30 +0530

    committed 11/16/2023, 2:52:30 PM

--- set05/problem03.c
+++ set05/problem03.c
@@ -8,17 +8,31 @@ struct camel {
 typedef struct camel Camel;
 
 Camel input();
-void find_weight(Camel *c); 
-void output(Camel c);
+void find_weight(Camel *C); 
+void output(Camel C);
 int main()
 {
-
+    Camel C;
+    float radius, height, length,weight;
+    C=input();
+    find_weight(&C);
+    output(C);
     return 0;
 }
 Camel input()
 {
     Camel C;
     printf("Enter the radius,height,length of the camel:\n");
-    scanf("%f,%f,%f",C.radius,C.height,C.weight);
+    scanf("%f %f %f",&C.radius,&C.height,&C.length);
     return C;
+}
+void find_weight(Camel *C)
+{
+    float weight, pi;
+    pi = 3.142;
+    C->weight = pi * C->radius * C->radius * C->radius * sqrt(C->height * C->length);
+}
+void output(Camel C)
+{
+    printf("The weight of the camel with radius: %0.1f, height: %0.1f, length: %0.1f is %f",C.radius,C.height,C.length,C.weight);
 }
