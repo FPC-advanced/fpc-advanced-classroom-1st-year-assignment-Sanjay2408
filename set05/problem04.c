6c36fbda45bdaf6abae0f76bc00d2a974c24a6a6
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-16 14:54:00 +0530

    committed 11/16/2023, 2:53:59 PM

---
+++ set05/problem04.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void input_camel_details(float *radius, float *height, float *length);
+int find_mood(float radius, float height, float length);
+void output(float radius, float height, float length, int mood);
+int main()
