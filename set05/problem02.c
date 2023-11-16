29511415af041e828f6fd6acc20ad5aa22525deb
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-16 14:11:23 +0530

    committed 11/16/2023, 2:11:22 PM

---
+++ set05/problem02.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length, float weight);
