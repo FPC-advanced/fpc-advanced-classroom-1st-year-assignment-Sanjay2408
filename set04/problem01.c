69a08069d124aa71edd845e8a77ec00c7073d6dc
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-16 17:56:49 +0530

    committed 11/16/2023, 5:56:47 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -29,6 +29,7 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
         *res_num = ( num1 * den2 ) + ( num2 * den1 );
         *res_den = (den1 * den2);
     }
+    for(i=2;i<)
 }
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
 {
