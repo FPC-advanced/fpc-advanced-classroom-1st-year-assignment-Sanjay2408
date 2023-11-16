cf23ae5e134e294032925b525afad25876746db2
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-16 14:59:00 +0530

    committed 11/16/2023, 2:58:59 PM

--- set05/problem04.c
+++ set05/problem04.c
@@ -8,11 +8,15 @@ int main()
     int mood;
     input_camel_details(&radius,&height,&length);
     mood=find_mood(radius,height,length);
-    output(radius,height,length,mood)
+    output(radius,height,length,mood);
     return 0;
 }
 void input_camel_details(float *radius, float *height, float *length)
 {
     printf("Enter the radius,height and length of the camel:\n");
     scanf("%f %f %f",radius,height,length);
+}
+int find_mood(float radius, float height, float length)
+{
+    
 }
