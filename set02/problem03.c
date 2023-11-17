efbeac5f7633ac0c61de64ebedb023ec46566beb
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 10:31:24 +0530

    committed 11/17/2023, 10:31:23 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -4,11 +4,11 @@ int is_composite(int n);
 void output(int n, int result);
 int main()
 {
- int n,result;
- n=input_number();
- result=is_composite(n);
- output(n,result);
- return 0;   
+   int n,result;
+   n=input_number();
+   result=is_composite(n);
+   output(n,result);
+   return 0;   
 }
 int input_number()
 {
