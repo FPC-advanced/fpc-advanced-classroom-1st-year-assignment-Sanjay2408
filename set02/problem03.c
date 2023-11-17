f19f90820d9280d339eb3634e95f5167e1808a8d
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 10:33:54 +0530

    committed 11/17/2023, 10:33:53 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -19,23 +19,23 @@ int input_number()
 }
 int is_composite(int n)
 {
-  int i;
-  if(n <= 1)
-  {
-   return 0;
-  }
-  if(n==2)
-  {
-   return 0;
-  }
-  for (i=2;i<=n/2;i++)
-  {
-   if (n%i == 0)
+   int i;
+   if(n <= 1)
    {
-      return 1;
+    return 0;
    }
-  }
-  return 0;
+   if(n==2)
+   {
+      return 0;
+   }
+   for (i=2;i<=n/2;i++)
+   {
+      if (n%i == 0)
+    {
+      return 1;
+    }
+   }
+   return 0;
 }
 
 void output(int n, int result)
