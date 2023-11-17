c681cb6a7f28a40c16430fbc8fa63be5ab1f2858
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 10:09:03 +0530

    committed 11/17/2023, 10:09:02 AM

--- set01/problem08.c
+++ set01/problem08.c
@@ -24,17 +24,17 @@ void input_array(int n, int a[n])
 {
   printf("Enter the array elements\n");
   for(int i=0;i<n;i++)
-  { 
-      scanf("%d",&a[i]);
+  {
+    scanf("%d",&a[i]);
   }
 }
 int sum_n_array(int n, int a[n])
 {
   int i,sum=0;
   for(i=0;i<n;i++)
-   {
-      sum=sum+a[i];
-   }
+  {
+    sum=sum+a[i];
+  }
   return sum;
 }
 void output(int n, int a[n],int sum)
