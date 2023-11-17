f195c055a79262749f7e4b839f47da3aaeb90225
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 09:27:21 +0530

    committed 11/17/2023, 9:27:20 AM

--- set01/problem05.c
+++ set01/problem05.c
@@ -4,20 +4,20 @@ int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
 int main()
 {
-    int a,b,c,largest,x;
-    a=input();
-    b=input();
-    c=input();
-    largest=compare(a,b,c);
-    output(a,b,c,largest);
-    return 0;
+  int a,b,c,largest,x;
+  a=input();
+  b=input();
+  c=input();
+  largest=compare(a,b,c);
+  output(a,b,c,largest);
+  return 0;
 }
 int input()
 {
- int x;
- printf("Enter a number\n");
- scanf("%d",&x);
- return x;
+  int x;
+  printf("Enter a number\n");
+  scanf("%d",&x);
+  return x;
 }
 int compare(int a,int b,int c)
 {
@@ -39,5 +39,5 @@ int compare(int a,int b,int c)
 }
 void output(int a, int b, int c, int largest)
 {
- printf("The largest of %d ,%d and %d is %d",a,b,c,largest);
+  printf("The largest of %d ,%d and %d is %d",a,b,c,largest);
 }
