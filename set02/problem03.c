668d8e78ba491e9ac62426e9e85afceec0abc5e8
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 10:32:54 +0530

    committed 11/17/2023, 10:32:53 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -29,11 +29,11 @@ int is_composite(int n)
    return 0;
   }
   for (i=2;i<=n/2;i++)
-     {
-        if (n%i == 0)
-       {
-          return 1;
-       }
+  {
+   if (n%i == 0)
+   {
+      return 1;
+   }
      }
        return 0;
   }
