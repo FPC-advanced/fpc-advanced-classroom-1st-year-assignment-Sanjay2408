9d43077bbf8159d8302234d004829fc996adcf48
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 10:10:33 +0530

    committed 11/17/2023, 10:10:32 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -20,10 +20,10 @@ void input_two_strings(char *string1, char *string2)
 }
 int stringcompare(char *string1, char *string2)
 {
- int i;
- for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
- {
-   if (string1[i]>string2[i])
+  int i;
+  for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
+  {
+    if (string1[i]>string2[i])
     {
       return 1;
     }
