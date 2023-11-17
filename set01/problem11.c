b607653bfe8ebd9b58ce473e31e9a9c43ec75205
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-17 10:17:03 +0530

    committed 11/17/2023, 10:17:02 AM

--- set01/problem11.c
+++ set01/problem11.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 struct _complex 
 {
-float real;
-float imaginary;
+ float real;
+ float imaginary;
 };
 typedef struct _complex Complex;
 Complex input_complex();
@@ -10,19 +10,19 @@ Complex add_complex(Complex a, Complex b);
 void output(Complex a, Complex b, Complex sum);
 int main()
 {
- Complex a,b,sum;
- a=input_complex();
- b=input_complex();
- sum=add_complex(a,b);
- output(a,b,sum);
- return 0;
+    Complex a,b,sum;
+    a=input_complex();
+    b=input_complex();
+    sum=add_complex(a,b);
+    output(a,b,sum);
+    return 0;
 }
 Complex input_complex()
 {
- Complex x;
- printf("Enter a real and imaginary number:\n");
- scanf("%f %f",&x.real,&x.imaginary);
- return x;
+    Complex x;
+    printf("Enter a real and imaginary number:\n");
+    scanf("%f %f",&x.real,&x.imaginary);
+    return x;
 }
 Complex add_complex(Complex a,Complex b)
 {
