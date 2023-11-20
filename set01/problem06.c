0a3c21618c07cbee581ef54e449f6f7c89084064
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-20 10:03:32 +0530

    committed 11/20/2023, 10:03:30 AM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
-void input(int *a,int *b,int *c);
-void compare(int a,int b,int c,int *largest);
-void output(int a,int b,int c,int largest);
+void input(int *a, int *b, int *c);
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
 int main()
-{ 
-  int a,b,c,x,largest;
-  input(&a,&b,&c);
-  compare(a,b,c,&largest);
-  output(a,b,c,largest);
+{
+  int a, b, c, x, largest;
+  input(&a, &b, &c);
+  compare(a, b, c, &largest);
+  output(a, b, c, largest);
   return 0;
 }
 void input(int *a, int *b, int *c)
 {
   int x;
   printf("Enter three numbers\n");
-  scanf("%d %d %d",a,b,c);
+  scanf("%d %d %d", a, b, c);
 }
 void compare(int a, int b, int c, int *largest)
 {
-  if(a>=b && a>=c)
+  if (a >= b && a >= c)
   {
-    *largest=a;
+    *largest = a;
+  }
+  else if (b >= c)
+  {
+    *largest = b;
   }
   else
-  if(b>=c)
   {
-    *largest=b;
-  }
-  else
-  {
-    *largest=c;
+    *largest = c;
   }
 }
-void output(int a,int b,int c,int largest)
+void output(int a, int b, int c, int largest)
 {
-  printf("the largest of %d,%d and %d is %d",a,b,c,largest);
+  printf("the largest of %d,%d and %d is %d", a, b, c, largest);
 }
--- set01/problem10.c
+++ set01/problem10.c
@@ -22,35 +22,22 @@ int stringcompare(char *string1, char *string2)
 {
   int i;
   for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
-  {
-    if (string1[i]>string2[i])
-    {
-      return 1;
-    }
-    else 
-    if(string1[i]<string2[i])
-    {
-      return 0;
-    }
-    else
-    {
-      return -1;
-    }
-  }
+  return (string1[i]-string2[i];
 }
 void output(char *string1, char *string2, int result)
  {
-  if(result==1)
-  {
-    printf("%s is greater than %s\n",string1,string2);
-  }
-  else
-  if(result==0)
-  {
-    printf("%s is greater than %s\n",string2,string1);
-  }
-  else
-  {
-    printf("Both the strings are equal %s=%s",string1,string2);
-  }
+  // if(result==1)
+  // {
+  //   printf("%s is greater than %s\n",string1,string2);
+  // }
+  // else
+  // if(result==0)
+  // {
+  //   printf("%s is greater than %s\n",string2,string1);
+  // }
+  // else
+  // {
+  //   printf("Both the strings are equal %s=%s",string1,string2);
+  // }
+  printf("%s",result);
 }
