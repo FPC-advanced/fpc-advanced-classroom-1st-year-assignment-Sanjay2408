03f29a5e4a75a06cf8b7b3255fd4b4f051208d9c
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-21 14:41:28 +0530

    committed 11/21/2023, 2:41:28 PM

--- set05/problem01.c
+++ set05/problem01.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-struct _point {
+struct _point
+{
   float x;
   float y;
 };
@@ -12,26 +13,26 @@ void dist(Point a, Point b, float *res);
 void output(Point a, Point b, float res);
 int main()
 {
-  Point a,b;
+  Point a, b;
   float res;
-  a=input();
-  b=input();
-  dist(a,b,&res);   
-  output(a,b,res);
+  a = input();
+  b = input();
+  dist(a, b, &res);
+  output(a, b, res);
   return 0;
 }
 Point input()
 {
   Point P;
   printf("Enter the coordinates of (x,y):");
-  scanf("%f  %f",&P.x,&P.y);
+  scanf("%f  %f", &P.x, &P.y);
   return P;
 }
 void dist(Point a, Point b, float *res)
 {
-  *res =sqrt( ( b.x - a.x ) * ( b.x - a.x )+( b.y - a.y ) * ( b.y - a.y ) );
+  *res = sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
 }
 void output(Point a, Point b, float res)
 {
-  printf("The distance between (%0.1f,%0.1f) and (%0.1f,%0.1f) is %0.1f\n",a.x,a.y,b.x,b.y,res);
+  printf("The distance between (%0.1f,%0.1f) and (%0.1f,%0.1f) is %0.1f\n", a.x, a.y, b.x, b.y, res);
 }
--- set05/problem02.c
+++ set05/problem02.c
@@ -5,25 +5,25 @@ float find_weight(float radius, float height, float length);
 void output(float radius, float height, float length, float weight);
 int main()
 {
-    float radius,height,length,weight;
-    input_camel_details(&radius,&height,&length);
-    weight=find_weight(radius,height,length);
-    output(radius,height,length,weight);
+    float radius, height, length, weight;
+    input_camel_details(&radius, &height, &length);
+    weight = find_weight(radius, height, length);
+    output(radius, height, length, weight);
     return 0;
 }
 void input_camel_details(float *radius, float *height, float *length)
 {
     printf("Enter the radius,height and length of the Camel:\n");
-    scanf("%f %f %f",radius,height,length);
+    scanf("%f %f %f", radius, height, length);
 }
 float find_weight(float radius, float height, float length)
 {
-    float weight,pi;
+    float weight, pi;
     pi = 3.142;
-    weight = pi * radius*radius*radius * sqrt(height * length);
+    weight = pi * radius * radius * radius * sqrt(height * length);
     return weight;
 }
 void output(float radius, float height, float length, float weight)
 {
-    printf("The weight of the camel with radius: %0.1f, height: %0.1f, length: %0.1f is %f",radius,height,length,weight);
+    printf("The weight of the camel with radius: %0.1f, height: %0.1f, length: %0.1f is %f", radius, height, length, weight);
 }
