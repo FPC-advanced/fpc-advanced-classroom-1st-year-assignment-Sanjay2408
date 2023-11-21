04de430adb95186fb95426a7252a65c6c23a6ee8
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-21 14:33:58 +0530

    committed 11/21/2023, 2:33:57 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
-int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
-void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3);
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result);
 int main()
 {
-float x1,y1,x2,y2,x3,y3;
-int result;
-input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
-result = is_triangle(x1,y1,x2,y2,x3,y3);
-output(x1,y1,x2,y2,x3,y3,result);
-return 0;
+  float x1, y1, x2, y2, x3, y3;
+  int result;
+  input_triangle(&x1, &y1, &x2, &y2, &x3, &y3);
+  result = is_triangle(x1, y1, x2, y2, x3, y3);
+  output(x1, y1, x2, y2, x3, y3, result);
+  return 0;
 }
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
 {
- printf("Enter the points of the triangle\n");
- scanf("%f %f",x1,y1);
- scanf("%f %f",x2,y2);
- scanf("%f %f",x3,y3);
+  printf("Enter the points of the triangle\n");
+  scanf("%f %f", x1, y1);
+  scanf("%f %f", x2, y2);
+  scanf("%f %f", x3, y3);
 }
-int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
 {
- float area;
- area=(0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2 ) ) );
- if(area==0)
+  float area;
+  area = (0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)));
+  if (area == 0)
   {
     return 1;
   }
- else
+  else
   {
     return 0;
   }
 }
-void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result)
 {
- if (result==1)
+  if (result == 1)
   {
-   printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f) do not form a triangle\n",x1,y1,x2,y2,x3,y3);
+    printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f) do not form a triangle\n", x1, y1, x2, y2, x3, y3);
   }
- else
+  else
   {
-   printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f)  forms a triangle\n",x1,y1,x2,y2,x3,y3);
+    printf(" The points (%0.1f,%0.1f),(%0.1f,%0.1f) and (%0.1f,%0.1f)  forms a triangle\n", x1, y1, x2, y2, x3, y3);
   }
-    
 }
