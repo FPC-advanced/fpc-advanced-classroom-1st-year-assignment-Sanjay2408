b984993aaae50e8842b03692a811ab8b6f46c006
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-21 14:41:58 +0530

    committed 11/21/2023, 2:41:57 PM

--- set05/problem03.c
+++ set05/problem03.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-struct camel {
-	float radius, height, length,weight;
+struct camel
+{
+    float radius, height, length, weight;
 };
 
 typedef struct camel Camel;
 
 Camel input();
-void find_weight(Camel *C); 
+void find_weight(Camel *C);
 void output(Camel C);
 int main()
 {
     Camel C;
-    float radius, height, length,weight;
-    C=input();
+    float radius, height, length, weight;
+    C = input();
     find_weight(&C);
     output(C);
     return 0;
@@ -23,7 +24,7 @@ Camel input()
 {
     Camel C;
     printf("Enter the radius,height,length of the camel:\n");
-    scanf("%f %f %f",&C.radius,&C.height,&C.length);
+    scanf("%f %f %f", &C.radius, &C.height, &C.length);
     return C;
 }
 void find_weight(Camel *C)
@@ -34,5 +35,5 @@ void find_weight(Camel *C)
 }
 void output(Camel C)
 {
-    printf("The weight of the camel with radius: %0.1f, height: %0.1f, length: %0.1f is %f",C.radius,C.height,C.length,C.weight);
+    printf("The weight of the camel with radius: %0.1f, height: %0.1f, length: %0.1f is %f", C.radius, C.height, C.length, C.weight);
 }
