8f35b7646564324d29d14826aff7b6c18313f29a
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-21 14:35:58 +0530

    committed 11/21/2023, 2:35:57 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,54 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
 int input_array_size();
 void init_array(int n, int a[n]);
 void erotosthenes_sieve(int n, int a[n]);
 void output(int n, int a[n]);
 int main()
 {
- int n = input_array_size(); 
- int a[n + 1];
- init_array(n,a);
- erotosthenes_sieve(n,a);
- output(n,a);
- return 0;
+    int n = input_array_size();
+    int a[n + 1];
+    init_array(n, a);
+    erotosthenes_sieve(n, a);
+    output(n, a);
+    return 0;
 }
 int input_array_size()
 {
- int n;
- printf("Enter the size of the array:");
- scanf("%d",&n);
- return n;
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d", &n);
+    return n;
 }
 void init_array(int n, int a[n])
 {
     int i;
-    for(i=2;i<n;i++)
+    for (i = 2; i < n; i++)
     {
-     a[i]=i;
+        a[i] = i;
     }
 }
 void erotosthenes_sieve(int n, int a[n])
 {
- int i;
- for(i=2;i*i<=n;i++)
- {
-  if(a[i] != 0)
-  {
-   for (int j=i*i;j <=n; j+=i)
-   {
-       a[j]=0;
-   }
-  }
- }
+    int i;
+    for (i = 2; i * i <= n; i++)
+    {
+        if (a[i] != 0)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                a[j] = 0;
+            }
+        }
+    }
 }
 void output(int n, int a[n])
 {
- for(int i=2;i<=n;i++)
- {
-  if(a[i] != 0)
-   {
-    printf("%d ",i);
-   }
- }
- printf("\n");
+    for (int i = 2; i <= n; i++)
+    {
+        if (a[i] != 0)
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
 }
