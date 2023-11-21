1305ddd7dd04c05e7ae6b08339ef2cf7c71af7fb
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-21 14:34:58 +0530

    committed 11/21/2023, 2:34:57 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -4,43 +4,43 @@ int is_prime(int n);
 void output(int n, int result);
 int main()
 {
- int n,result;
- n = input_number();
- result=is_prime(n);
- output(n,result);
- return 0;
+  int n, result;
+  n = input_number();
+  result = is_prime(n);
+  output(n, result);
+  return 0;
 }
 int input_number()
 {
- int n;
- printf("Enter a number: ");
- scanf("%d",&n);
- return n;
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  return n;
 }
 int is_prime(int n)
 {
- int i,result;
- if(n<=1)
+  int i, result;
+  if (n <= 1)
   {
     return 0;
   }
- for(i=2;i*i<=n;i++)
- {
-  if(n%i== 0)
-   {
-    return 0;
-   } 
- }
+  for (i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      return 0;
+    }
+  }
   return 1;
 }
 void output(int n, int result)
 {
- if (result==1)
- {
-  printf("%d is a prime number",n);
- }
- else
- {
-  printf("%d is not a prime number",n);
- }
+  if (result == 1)
+  {
+    printf("%d is a prime number", n);
+  }
+  else
+  {
+    printf("%d is not a prime number", n);
+  }
 }
