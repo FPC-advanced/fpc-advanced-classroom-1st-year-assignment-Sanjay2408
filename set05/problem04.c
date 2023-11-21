7f6a4e4daf4b92082ff8101caee03a93e2bcf3bf
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-11-21 14:42:28 +0530

    committed 11/21/2023, 2:42:27 PM

--- set05/problem04.c
+++ set05/problem04.c
@@ -4,26 +4,25 @@ int find_mood(float radius, float height, float length);
 void output(float radius, float height, float length, int mood);
 int main()
 {
-    float radius,height,length;
+    float radius, height, length;
     int mood;
-    input_camel_details(&radius,&height,&length);
-    mood=find_mood(radius,height,length);
-    output(radius,height,length,mood);
+    input_camel_details(&radius, &height, &length);
+    mood = find_mood(radius, height, length);
+    output(radius, height, length, mood);
     return 0;
 }
 void input_camel_details(float *radius, float *height, float *length)
 {
     printf("Enter the radius,height and length of the camel:\n");
-    scanf("%f %f %f",radius,height,length);
+    scanf("%f %f %f", radius, height, length);
 }
 int find_mood(float radius, float height, float length)
 {
-    if(radius<height && radius<length)
+    if (radius < height && radius < length)
     {
         return 0;
     }
-    else
-    if(height<length)
+    else if (height < length)
     {
         return 1;
     }
@@ -34,18 +33,16 @@ int find_mood(float radius, float height, float length)
 }
 void output(float radius, float height, float length, int mood)
 {
-    if(mood == 0)
+    if (mood == 0)
     {
         printf("Camel is Sick \n");
     }
-    else 
-    if(mood == 1)
+    else if (mood == 1)
     {
         printf("Camel is Happy \n");
-    } 
-    else
-    if(mood == -1)
+    }
+    else if (mood == -1)
     {
         printf("Camel is Tense \n");
-    } 
     }
+}
