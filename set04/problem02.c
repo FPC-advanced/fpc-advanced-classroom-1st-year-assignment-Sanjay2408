0d0bc0ae875ce13bf9555e1e899ec8b33f358d65
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-05 07:04:07 +0530

    committed 12/5/2023, 7:04:06 AM

--- set04/problem02.c
+++ set04/problem02.c
@@ -8,7 +8,7 @@ int main()
     x=input();
     y=input();
     z=input();
-    smallest=
+    smallest=smallest2(x,y,z);
 }
 Fraction input()
 {
@@ -35,7 +35,7 @@ int lcm2(int a,int b,int c)
 {
     return lccm(lcm(a,b),c);
 }
-Fraction smallest(Fraction x,Fraction y,Fraction z)
+Fraction smallest2(Fraction x,Fraction y,Fraction z)
 {
  int a=(x.num * y.den * z.den);
  int b=(y.num * x.den * z.den);
