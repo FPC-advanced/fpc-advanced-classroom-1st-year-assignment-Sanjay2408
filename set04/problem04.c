8aeba253991a07d5ab11e0b05355cf3c08bd986c
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-05 10:42:42 +0530

    committed 12/5/2023, 10:42:42 AM

--- set04/problem04.c
+++ set04/problem04.c
@@ -1,52 +1,52 @@
 #include <stdio.h>
 int input_degree();
-void input_coefficients(int n, float a[n+1]);
+void input_coefficients(int n, float a[n + 1]);
 float input_x();
-float evaluate_polynomial(int n, float a[n+1], float x);
-void output(int n, float a[n+1], float x, float result);
+float evaluate_polynomial(int n, float a[n + 1], float x);
+void output(int n, float a[n + 1], float x, float result);
 int main()
 {
     int n;
-    n=input_degree();
-    float a[n+1],z=0,x=0;
-    input_coefficients(n,a);
-    x=input_x();
-    z = evaluate_polynomial(n,a,x);
-    output(n,a,x,z);
+    n = input_degree();
+    float a[n + 1], z = 0, x = 0;
+    input_coefficients(n, a);
+    x = input_x();
+    z = evaluate_polynomial(n, a, x);
+    output(n, a, x, z);
     return 0;
 }
 int input_degree()
 {
     int n;
     printf("Enter degree:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     return n;
 }
-void input_coefficients(int n, float a[n+1])
+void input_coefficients(int n, float a[n + 1])
 {
-    printf("Enter %d coefficients:\n",n+1);
-    for(int i=0;i<(n+1);i++)
+    printf("Enter %d coefficients:\n", n + 1);
+    for (int i = 0; i < (n + 1); i++)
     {
-        scanf("%f",&a[i]);
+        scanf("%f", &a[i]);
     }
 }
 float input_x()
 {
     float x;
     printf("Enter the number you want to evaluate:\n");
-    scanf("%f",&x);
+    scanf("%f", &x);
     return x;
 }
-float evaluate_polynomial(int n, float a[n+1], float x)
+float evaluate_polynomial(int n, float a[n + 1], float x)
 {
-    float z=a[n];
-    for(int i=n-1;i>=0;i--)
+    float z = a[n];
+    for (int i = n - 1; i >= 0; i--)
     {
-        z=(z*x)+a[i];
+        z = (z * x) + a[i];
     }
     return z;
 }
-void output(int n, float a[n+1], float x, float result)
+void output(int n, float a[n + 1], float x, float result)
 {
-    printf("%.2f\n",result);
+    printf("%.2f\n", result);
 }
