f95c195489184c2c12ae73c9ceeff4f1b4203c99
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-05 07:28:15 +0530

    committed 12/5/2023, 7:28:14 AM

--- set04/problem03.c
+++ set04/problem03.c
@@ -8,7 +8,6 @@ int main()
  input_n_and_r(&n,&r);
  ncr = nCr(n,r);
  output(n,r,ncr);
-
  return 0;
 }
 void input_n_and_r(int *n, int *r)
@@ -22,6 +21,6 @@ int nCr(int n, int r)
 }
 void output(int n, int r, int result)
 {
-
+ printf("for n=%d and r=%d,n")
 }
 
