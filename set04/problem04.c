1b5ad3518bc86be1740d123d070b1de9b795a2d7
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-05 07:32:15 +0530

    committed 12/5/2023, 7:32:14 AM

--- set04/problem04.c
+++ set04/problem04.c
@@ -4,3 +4,4 @@ void input_coefficients(int n, float a[n]);
 float input_x();
 float evaluate_polynomial(int n, float a[n], float x);
 void output(int n, float a[n], float x, float result);
+int main()
