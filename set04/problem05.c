e324c19d0fcb56da335525ef89bd5d0be91f5e82
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-05 07:47:16 +0530

    committed 12/5/2023, 7:47:15 AM

--- set04/problem05.c
+++ set04/problem05.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+int input_size();
+void input_array(int n, int a[n]);
+int find_largest_index(int n, int a[n]);
+void output(int index);
+int main()
+{
+    return 0;
+}
+int input_size()
+{
+    
+}
