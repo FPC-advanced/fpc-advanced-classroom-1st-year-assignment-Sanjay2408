d31b1a9b2c082c2190e0654d498cdcbc792ac4e0
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-05 07:40:16 +0530

    committed 12/5/2023, 7:40:15 AM

--- set04/problem04.c
+++ set04/problem04.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int input_degree();
-void input_coefficients(int n, float a[n]);
+void input_coefficients(int n, float a[n+1]);
 float input_x();
 float evaluate_polynomial(int n, float a[n], float x);
 void output(int n, float a[n], float x, float result);
@@ -39,7 +39,7 @@ float evaluate_polynomial(int n, float a[n], float x)
     }
     return z;
 }
-void output(int n, float a[n], float x, float result)
+void output(int n, float a[n+1], float x, float result)
 {
     printf("")
 }
