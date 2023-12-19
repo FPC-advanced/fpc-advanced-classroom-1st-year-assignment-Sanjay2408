c9ea1880dc94c4bfbd2680a2f0a879f9e64b7ae4
Sanjay2408 <141307989+Sanjay2408@users.noreply.github.com>
2023-12-19 14:28:13 +0530

    committed 12/19/2023, 2:28:12 PM

--- psc_see/problem01.c
+++ psc_see/problem01.c
@@ -1,55 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
 int input();
 int factorial(int x);
-void output(int x,int fact);
+void output(int x, int fact);
 int main()
 {
-    int x,fact;
-    x=input();
+    int x, fact;
+    x = input();
     fact = factorial(x);
-    output(x,fact);
+    output(x, fact);
     return 0;
 }
 int input()
 {
     int x;
     printf("Enter the number to find its factorial:\n");
-    scanf("%d",&x);
+    scanf("%d", &x);
     return x;
 }
 int factorial(int x)
 {
     int fact;
-    if ((x==1))
-        {
-            return 0;
-        }
-    for(int i=1;i<=x/2;i++)
+    if ((x == 1))
     {
-    if ((x==2))
+        return 0;
+    }
+    for (int i = 1; i <= x / 2; i++)
+    {
+        if ((x == 2))
         {
             return 2;
             break;
         }
-     else
-     if (x%i == 0)
-    {
-        fact = i ;
+        else if (x % i == 0)
+        {
+            fact = i;
+        }
+        else
+        {
+            break;
+        }
     }
-    else
-    {
-        break;
-    }
- }
 
-return fact;
+    return fact;
 }
-void output(int x,int fact)
+void output(int x, int fact)
 {
-    if(fact != 1)
+    if (fact != 1)
     {
-        printf("The factorial of %d(%d!)\n",x,fact);
+        printf("The factorial of %d(%d!)\n", x, fact);
     }
     else
-    printf("%d is not a factorial number\n",x);
+        printf("%d is not a factorial number\n", x);
 }
