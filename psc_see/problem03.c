4e0a393d694e2a31b58fe8f970dc1320038bb43d
sanjay <sanjays.bsc23@rvu.edu.in>
2024-03-11 22:03:28 +0530

    committed 3/11/2024, 10:03:28 PM

--- psc_see/problem03.c
+++ psc_see/problem03.c
@@ -1,270 +1,151 @@
-// #include<stdio.h>
-// typedef struct Fritacole{
-// char name;
-// int height;
-// int integrity;
-// int interest;
-// int ability;
-// int discipline;
-// } fritacole;
-
-// typedef struct Teams{
-// int a;
-// int b;
-// }team;
-
-// typedef struct Game
-// {
-// int point;
-// int foul;
-// } game;
-// void input();
-// fritacole input_players();
-// team sort();
-// game input_n(team a,team b);
-// int compare_fritacoles();
-// team compare_teams(team a,team b);
-// void verify_win();
-// void output(team win);
-// int main()
-// {
-
-//     return 0;
-// }
-//  void input()
-// {
-//     fritacole n;
-//     printf("Enter the player name:");
-//     scanf("%s",&n.name);
-//     printf("Enter the player height:");
-//     scanf("%d",n.height);
-//     printf("Enter the player integrity out of 5");
-//     scanf("%d",n.integrity);
-//     printf("Enter the player interest out of 5");
-//     scanf("%d",n.interest);
-//     printf("Enter the player ability out of 5");
-//     scanf("%d",n.ability);
-//     printf("Enter the player discipline out of 5");
-//     scanf("%d",n.discipline);
-// }
-// fritacole input_players(int n)
-// {
-// printf("Enter the no of players:\n");
-// scanf("%d",&n);
-// for(int i=0;i<n;i++)
-// {
-//     input(i);
-// }
-// }
-// team sort()
-// {
-//     team a,b;
-// }
-// game input_n(team a,team b)
-// {
-//     game goals,fouls;
-//     printf("Enter the goals scored by team A:\n");
-//     scanf("%d",&goals.a);
-//     printf("Enter the goals scored by team A:\n");
-//     scanf("%d",&fouls.a);
-//     printf("Enter the goals scored by team B:\n");
-//     scanf("%d",&goals.b);
-//     printf("Enter the goals scored by team B:\n");
-//     scanf("%d",&fouls.b);
-// }
-// team compare_teams(team a,team b);
-// {
-//     if 
-// }
-
-// A Fritacole has attributes: name, height,  integrity, interest, ability, discipline.
-// There were two teams of Fritacoles playing a freyball game.
-// A freyball game consists of points and fouls
-// Each point is scored by a fritacole.
-// Each foul is committed by a fritacole.
-// The team that scores more goals wins the game.
-// If the goals are the same, the team that commits less fouls wins the game.
-
-// a. Write a function to verify whether the better team won a game?
-// Note that taller the person, the more chance that the fritacole may score a point.
-
-// b, Write a function to convert a fritacole into a string.
-
-// c. Write a function to convert a string into a fritacole.
-// Hints:
-// Use structures for Fritacole, team, game, points and fouls.
-// Write functions to verify_win, compare_fritacoles, compare_teams and other functions.
-
-
 #include <stdio.h>
-int frita_jersey();
+#include <string.h>
 
+// Define a structure for an ID
+typedef struct {
+    int fritid;
+} FritID;
 
-typedef struct _fritacole {
-  int frita_jersey;
-  char name[100];
-  float height;
-} fritacole;
+// Define a structure for a fritacole
+typedef struct {
+    FritID fritid;
+    char name[100];
+    float height;
+    int integrity;
+    int interest;
+    int ability;
+    int discipline;
+} Fritacole;
 
-int frita_jersey(){
-  static int f_jersey = -1;
-  f_jersey++;
-  return f_jersey;
+// Define a structure for a point
+typedef struct {
+    int goals_scored;
+} Point;
+
+// Define a structure for a foul
+typedef struct {
+    int fouls_committed;
+} Foul;
+
+// Define a structure for a team
+typedef struct {
+    char name[100];
+    int num_players;
+    Fritacole players[20];
+    Point points;
+    Foul fouls;
+} Team;
+
+// Define a structure for a game
+typedef struct {
+    Team teams[2];
+} Game;
+
+// Function to compare two fritacoles based on their heights
+int compare_fritacoles(Fritacole a, Fritacole b) {
+    return (a.height > b.height) ? 1 : ((a.height < b.height) ? -1 : 0);
 }
 
-typedef struct foul {
-  int frita_jersey;
-  char name[100];
-} fouls;
-
-typedef struct points {
-int frita_jersey;
-char name[100];
-} points;
-
-typedef struct _team {
-  char name[100];
-  int numplayers;
-  fritacole players[20];
-  fouls fouls[20];
-  points points[20];
-  int numfouls;
-  int numpoints;
-} team;
-
-typedef struct _game {
-  int jersey;
-  team teams[2];
-} game;
-
-
-
-void inpfricta(fritacole *a) {
-  printf("Enter the name of the fritacole\n");
-  scanf("%s", a->name);
-  printf("Enter the height of the fritacole\n");
-  scanf("%f", &a->height);
+// Function to input details for a fritacole
+void input_fritacole(Fritacole *frita, int frita_id) {
+    frita->fritid.fritid = frita_id;
+    printf("Enter name of Fritacole %d: ", frita_id);
+    scanf("%s", frita->name);
+    printf("Enter height of Fritacole %d: ", frita_id);
+    scanf("%f", &frita->height);
+    printf("Enter integrity of Fritacole %d: ", frita_id);
+    scanf("%d", &frita->integrity);
+    printf("Enter interest of Fritacole %d: ", frita_id);
+    scanf("%d", &frita->interest);
+    printf("Enter ability of Fritacole %d: ", frita_id);
+    scanf("%d", &frita->ability);
+    printf("Enter discipline of Fritacole %d: ", frita_id);
+    scanf("%d", &frita->discipline);
 }
 
-
-
-void inpteam(team *a, int numplayers) {
-  a->numplayers = numplayers;
-  printf("Enter the name of the team\n");
-  scanf("%s", a->name);
-  for (int i = 0; i < a->numplayers; i++) {
-    a->players[i].frita_jersey=frita_jersey();
-    inpfricta(&a->players[i]);
-  }
+// Function to input details for a team
+void input_team(Team *team, int team_number) {
+    printf("Enter name of Team %d: ", team_number);
+    scanf("%s", team->name);
+    printf("Enter number of players in Team %d: ", team_number);
+    scanf("%d", &team->num_players);
+    // Input details for each player in the team
+    for (int i = 0; i < team->num_players; i++) {
+        input_fritacole(&team->players[i], i + 1);
+    }
+    printf("Enter number of goals scored by Team %d: ", team_number);
+    scanf("%d", &team->points.goals_scored);
+    printf("Enter number of fouls committed by Team %d: ", team_number);
+    scanf("%d", &team->fouls.fouls_committed);
 }
 
-
-void inpgame(game *a) {
-  printf("please enter game number: \n");
-  scanf("%d",&a->jersey);
-  int numplayers;
-  printf("Enter the number of players for each team\n");
-  scanf("%d", &numplayers);
-  for (int i = 0; i < 2; i++) {
-    inpteam(&a->teams[i], numplayers);
-  }
+// Function to input details for a game
+void input_game(Game *game) {
+    int num_teams;
+    printf("Enter the number of teams: ");
+    scanf("%d", &num_teams);
+    for (int i = 0; i < num_teams; i++) {
+        input_team(&game->teams[i], i + 1);
+    }
 }
 
-
-
-int inpfouls(game *a,int j) {
-  scanf("%d", &a->teams[j].numfouls);
-  for (int i = 0; i < a->teams[j].numfouls; i++) {
-    printf("Enter the jersey of the fritacole who fouled: \n");
-    scanf("%d",&a->teams[j].fouls[i].frita_jersey);
-  }
-  return a->teams[j].numfouls;
+// Function to find the better team among an array of teams
+Team find_better_team(Team *teams, int num_teams) {
+    Team better_team = teams[0];
+    for (int i = 1; i < num_teams; i++) {
+        if (teams[i].points.goals_scored > better_team.points.goals_scored) {
+            better_team = teams[i];
+        }
+    }
+    return better_team;
 }
 
+// Function to output game statistics including the result
+void output(Game *game, Team better_team) {
+    printf("\nGAME STATS:\n\n");
+    for (int i = 0; i < 2; i++) {
+        printf("Team %d: %s\n", i + 1, game->teams[i].name);
+        printf("Goals: %d\n", game->teams[i].points.goals_scored);
+        printf("Fouls: %d\n\n", game->teams[i].fouls.fouls_committed);
+    }
 
-int inppoints(game *a,int j) {
-  scanf("%d", &a->teams[j].numpoints);
-  for (int i = 0; i < a->teams[j].numpoints; i++) {
-    printf("Enter the jersey of the fritacole who scored: \n");
-    scanf("%d", &a->teams[j].points[i].frita_jersey);
-  }
-  return a->teams[j].numpoints;
+    // Check if the better team won or not
+    int better_team_index = -1;
+    for (int i = 0; i < 2; i++) {
+        if (strcmp(game->teams[i].name, better_team.name) == 0) {
+            better_team_index = i;
+            break;
+        }
+    }
+    if (better_team_index == -1) {
+        printf("Better team not found.\n");
+        return;
+    }
+
+    int better_team_won = 1;
+    for (int i = 0; i < 2; i++) {
+        if (i != better_team_index && game->teams[i].points.goals_scored >= better_team.points.goals_scored) {
+            better_team_won = 0;
+            printf("%s won the game.\n", game->teams[i].name);
+            break;
+        }
+    }
+    if (better_team_won) {
+        printf("%s won the game.\n", better_team.name);
+    }
 }
 
+int main() {
+    Game game;
 
+    printf("Enter details for the game:\n");
+    input_game(&game);
 
-float sumheight(game *a, int j) {
-  float hsum = 0;
-  for (int i = 0;i<a->teams[j].numplayers; i++) {
-    hsum += a->teams[j].players[i].height;
-  }
-  return hsum;
-}
+    Team better_team = find_better_team(game.teams, 2);
 
+    printf("The better team is: %s\n", better_team.name);
 
+    output(&game, better_team);
 
-void verifyadvantage(game *a) {
-  float team1height = sumheight(a, 0);
-  float team2height = sumheight(a, 1);
-  (team1height > team2height)   ? printf("team one has the advantage\n")
-  : (team1height < team2height) ? printf("team two has the advantage\n")
-                                : printf("both teams possess equal advantage\n");
-  ;
-}
-
-
-void verify_win(game *a) {
-  printf("team 1 info: \n");
-  for(int i=0;i<a->teams[0].numplayers;i++){
-    printf("name: %s ,jersey no: %d\n",a->teams[0].players[i].name,a->teams[0].players[i].frita_jersey);
-  }
-  printf("team 2 info: \n");
-  for(int i=0;i<a->teams[1].numplayers;i++){
-    printf("name: %s ,jersey no: %d\n",a->teams[1].players[i].name,a->teams[1].players[i].frita_jersey);
-  }
-
-  printf("\n\nenter the points for team %s: \n", a->teams[0].name);
-  int team1points = inppoints(a,0);
-  printf("enter the points for team %s: \n", a->teams[1].name);
-  int team2points = inppoints(a,1);
-  printf("enter the fouls for team %s: \n", a->teams[0].name);
-  int team1fouls = inpfouls(a,0);
-  printf("enter the fouls for team %s: \n", a->teams[1].name);
-  int team2fouls = inpfouls(a,1);
-  verifyadvantage(a);
-  (team1points>team2points)?printf("team one has won the game\n"):(team1points<team2points)?printf("team 2 has won the game\n"):
-  (team1fouls>team2fouls)?printf("both teams have equal points but since team 1 has more fouls, team 2 won\n"):(team1fouls<team2fouls)?
-  printf("both teams have equal points but since team 2 scored more fouls, team 1 won:\n"):printf("its a draw\n");
-}
-
-
-
-void output(game *a){
-  printf("\nGAME STATS: \n\n");
-  printf("The number of fouls scored by each team are: \n");
-  printf("Team 1: %d\n",a->teams[0].numfouls);
-  for(int i=0;i<a->teams[0].numfouls;i++){
-    printf("name: %s, jersey: %d\n",a->teams[0].players[a->teams[0].fouls[i].frita_jersey].name,a->teams[0].fouls[i].frita_jersey);
-  }
-  printf("Team 2: %d\n",a->teams[1].numfouls);
-  for(int i=0;i<a->teams[1].numfouls;i++){
-    printf("name: %s, jersey: %d\n",a->teams[1].players[a->teams[1].fouls[i].frita_jersey].name,a->teams[1].fouls[i].frita_jersey);
-  }
-  printf("\nThe number of points scored by each team are: \n");
-  printf("\nTeam 1: %d\n",a->teams[0].numpoints);
-  for(int i=0;i<a->teams[0].numpoints;i++){
-    printf("name: %s, jersey: %d\n",a->teams[0].players[a->teams[0].points[i].frita_jersey].name,a->teams[0].points[i].frita_jersey);
-  }
-  printf("\nTeam 2: %d\n",a->teams[1].numpoints);
-  for(int i=0;i<a->teams[1].numpoints;i++){
-    printf("name: %s, jersey: %d \n",a->teams[1].players[a->teams[1].points[i].frita_jersey].name,a->teams[1].points[i].frita_jersey);
-  }
-
-}
-
-int main(){
-  game a;
-  inpgame(&a);
-  verify_win(&a);
-  output(&a);
+    return 0;
 }
